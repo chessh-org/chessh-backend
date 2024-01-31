262e315f695df27b4512cf09c87210d03996291f
Nate Choe <nate@natechoe.dev>
2024-01-31 15:04:14 -0600

    API skeleton

--- src/client/api.c
+++ src/client/api.c
@@ -15,8 +15,51 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  * */
 
+#include <stdlib.h>
+
+#include <util.h>
 #include <client/frontend.h>
 
+static char *get_move(void *aux, enum player player);
+static void report_error(void *aux, int code);
+static void report_msg(void *aux, int msg_code);
+static void display_board(void *aux, struct game *game, enum player player);
+
 struct frontend *new_api_frontend(void) {
+	struct frontend *ret;
+
+	if ((ret = malloc(sizeof *ret)) == NULL) {
+		return NULL;
+	}
+
+	ret->get_move = get_move;
+	ret->report_msg = report_msg;
+	ret->report_error = report_error;
+	ret->display_board = display_board;
+	ret->free = (void (*)(struct frontend *)) free;
+	ret->aux = NULL;
+
+	return ret;
+}
+
+static char *get_move(void *aux, enum player player) {
+	UNUSED(aux);
+	UNUSED(player);
 	return NULL;
+}
+
+static void report_error(void *aux, int code) {
+	UNUSED(aux);
+	UNUSED(code);
+}
+
+static void report_msg(void *aux, int msg_code) {
+	UNUSED(aux);
+	UNUSED(msg_code);
+}
+
+static void display_board(void *aux, struct game *game, enum player player) {
+	UNUSED(aux);
+	UNUSED(game);
+	UNUSED(player);
 }
